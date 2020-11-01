e9f9901d7eeed839813d5b2db343e3f0a6805f39
AnnabelZhang1 <azhang22@stuy.edu>
2020-11-01 15:03:29 -0500

    added classify, rounded, and toString

--- Triangle.java
+++ Triangle.java
@@ -10,6 +10,13 @@ public class Triangle{
       v3 = c;
     }
 
+  public Sides()
+    {
+      a = v1.distanceTo(v2);
+      b = v2.distanceTo(v3);
+      c = v3.distanceTo(v1);
+    }
+
   public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
     {
       v1 = new Point(x1, y1);
@@ -20,29 +27,42 @@ public class Triangle{
 //accessors
   public double getPerimeter()
     {
-      a = v1.distanceTo(v2);
-      b = v2.distanceTo(v3);
-      c = v3.distanceTo(v1);
+      Sides();
       return a + b + c;
     }
 
   public double getArea()
     {
-      a = v1.distanceTo(v2);
-      b = v2.distanceTo(v3);
-      c = v3.distanceTo(v1);
-      s = (a + b + c)/2;
+      Sides();
+      double s = (a + b + c)/2;
       return Math.sqrt(s(s-a)(s-b)(s-c));
+    }
+
+  public double rounded(double n)
+    {
+      n = Math.round(n*10000)/10000;
     }
 
   public String classfiy()
     {
-      ~~
+      Sides();
+      a1 = rounded(a);
+      b1 = rounded(b);
+      c1 = rounded(c);
+
+      if a1 == b1 && b1 == c1
+        return "Equilateral";
+      else if a1 == b1 || b1 == c1
+        return "Isosceles";
+      else
+        return "Scalene";
     }
 
   public String toString()
     {
-      ~~
+      return "v1(" + v1.getX() + ", " + v1.getY() + ") "
+           + "v2(" + v2.getX() + ", " + v2.getY() + ") "
+           + "v3(" + v3.getX() + ", " + v3.getY() + ")";
     }
 
 //mutator
